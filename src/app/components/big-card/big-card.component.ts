333578fec98fb91c4689466f7f3bdeb531a6a3a2
Dahruz <dahruz.23@gmail.com>
2024-02-25 04:32:46 -0300

    componentes dinamicos

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
-
+  @Input()
+  photoCover:string="https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular-1080x675.png"
+  @Input()
+  cardTitle:string="Nova Versão"
+  @Input()
+  cardDescription:string="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+  
   constructor() { }
 
   ngOnInit(): void {
